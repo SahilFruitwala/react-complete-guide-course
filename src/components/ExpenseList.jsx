a919ca964e23c4b30240987dace77c8d04ce4366
SahilFruitwala <sahilfruitwala007@gmail.com>
2022-02-03 13:53:28 -0500

    Pass Children and classes to a component

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,26 +1,41 @@
-import "./ExpenseList.css"
-import ExpenseItem from './ExpenseItem';
+import "./ExpenseList.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 
-const ExpenseList = props => {
-	return (
-		<div className="expenses">
-			<ExpenseItem
-				title={props.expenses[0]?.title}
-				amount={props.expenses[0]?.amount}
-				date={props.expenses[0]?.date}
-			/>
-			<ExpenseItem
-				title={props.expenses[1]?.title}
-				amount={props.expenses[1]?.amount}
-				date={props.expenses[1]?.date}
-			/>
-			<ExpenseItem
-				title={props.expenses[2]?.title}
-				amount={props.expenses[2]?.amount}
-				date={props.expenses[2]?.date}
-			/>
-		</div>
-	);
+const ExpenseList = (props) => {
+  return (
+    <Card className="expenses">
+      {props.expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+      {/* {
+      props.expenses.map((expense) => {
+         return ( <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        )}
+      )} */}
+
+      {/* <ExpenseItem
+        title={props.expenses[1]?.title}
+        amount={props.expenses[1]?.amount}
+        date={props.expenses[1]?.date}
+      />
+      <ExpenseItem
+        title={props.expenses[2]?.title}
+        amount={props.expenses[2]?.amount}
+        date={props.expenses[2]?.date}
+      /> */}
+    </Card>
+  );
 };
 
 export default ExpenseList;
