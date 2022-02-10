b172b0373a4fd9806caba26a16f087f2296cc8a4
SahilFruitwala <sahilfruitwala007@gmail.com>
2022-02-09 19:09:43 -0500

    Learned how to use Styled Components & CSS Modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import './index.css';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
